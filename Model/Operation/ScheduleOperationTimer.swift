3fb148b38ca915ef30d727910712a0aebe924bf2
Thomas Evensen <thomeven@gmail.com>
2018-06-27 06:39:08 +0200

    minor fix

--- Model/Operation/ScheduleOperationTimer.swift
+++ Model/Operation/ScheduleOperationTimer.swift
@@ -67,7 +67,8 @@ final class ScheduleOperationTimer: SecondsBeforeStart, SetSortedAndExpanded, Se
             updatestatuslightDelegate?.updatestatuslight(color: .red)
             return
         }
-        self.logDelegate?.addlog(logrecord: "Timer: setting next scheduled task in: " + String(format: "%.0f", seconds) + " seconds...")
+        let timestring = Tools().timeString(seconds)
+        self.logDelegate?.addlog(logrecord: "Timer: setting next scheduled task in: " + timestring)
         self.timerTaskWaiting = Timer.scheduledTimer(timeInterval: seconds, target: self, selector: #selector(executetask), userInfo: nil, repeats: false)
         ViewControllerReference.shared.scheduledTask = self.sortedandexpanded?.getfirstscheduledtask()
         ViewControllerReference.shared.timerTaskWaiting = self.timerTaskWaiting
