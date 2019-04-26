7d6e90cd8dd6b2502a0c9e3b359abd6e7450383c
Thomas Evensen <thomeven@gmail.com>
2019-04-26 15:36:18 +0200

    refactor and cleanup

--- Model/Conversions/ConvertUserconfiguration.swift
+++ Model/Conversions/ConvertUserconfiguration.swift
@@ -5,6 +5,7 @@
 //  Created by Thomas Evensen on 26/04/2019.
 //  Copyright © 2019 Thomas Evensen. All rights reserved.
 //
+//  swiftlint:disable function_body_length cyclomatic_complexity
 
 import Foundation
 
