211332d81f28d4135007ac4f94833da5d4e6f833
Thomas Evensen <thomeven@gmail.com>
2019-05-07 20:37:55 +0200

    Localization

--- Model/Tools/Dateandtime.swift
+++ Model/Tools/Dateandtime.swift
@@ -36,20 +36,21 @@ final class Dateandtime {
         let (min, secf) = modf (60 * minf)
         // hr, min, 60 * secf
         if hr == 0 && min == 0 {
-            result = String(format: "%.0f", 60 * secf) + " secs"
+            result = String(format: "%.0f", 60 * secf) + " " + NSLocalizedString("secs", comment: "datetime")
         } else if hr == 0 && min < 60 {
-            result = String(format: "%.0f", min) + " mins " + String(format: "%.0f", 60 * secf) + " secs"
+            result = String(format: "%.0f", min) + " " + NSLocalizedString("mins", comment: "datetime")
+                + String(format: "%.0f", 60 * secf) + " " + "secs"
         } else if hr < 25 {
-            result = String(format: "%.0f", hr) + " hours " + String(format: "%.0f", min) + " mins"
+            result = String(format: "%.0f", hr) + " " +  NSLocalizedString("hours", comment: "datetime") + String(format: "%.0f", min) + " " + "mins"
         } else {
-            result = String(format: "%.0f", hr/24) + " days"
+            result = String(format: "%.0f", hr/24) + " " + NSLocalizedString("days", comment: "datetime")
         }
         if secf <= 0 {
-            result = " ... working ... "
+            result = "... working ..."
         }
         return result!
     }
-
+    
     // Calculation of time to a spesific date
     // Used in view of all tasks
     func timeString (_ seconds: Double) -> String {
@@ -62,9 +63,10 @@ final class Dateandtime {
         } else if hr == 0 && min < 60 {
             result = String(format: "%.0f", min) + "m " + String(format: "%.0f", 60 * secf) + "s"
         } else if hr < 25 {
-            result = String(format: "%.1f", hr) + "h " + String(format: "%.0f", min) + "m"
+            result = String(format: "%.0f", hr) + NSLocalizedString("h", comment: "datetime")  + " "
+                + String(format: "%.0f", min) + "m"
         } else {
-            result = String(format: "%.1f", hr/24) + "d"
+            result = String(format: "%.0f", hr/24) + "d"
         }
         return result ?? ""
     }
