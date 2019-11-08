fdeb037e09c009d35eca976af708191d1b5d6bfc
Thomas Evensen <thomeven@gmail.com>
2019-11-08 18:05:21 +0100

    Update ReadWriteDictionary.swift

--- Model/Fileoperations/ReadWriteDictionary.swift
+++ Model/Fileoperations/ReadWriteDictionary.swift
@@ -24,7 +24,7 @@ enum WhatToReadWrite {
 class ReadWriteDictionary: Setlog {
 
     // Name set for schedule, configuration or config
-    private var name: String?
+    private var plistname: String?
     // key in objectForKey, e.g key for reading what
     private var key: String?
     // Which profile to read
@@ -49,10 +49,10 @@ class ReadWriteDictionary: Setlog {
         if let profile = self.profile {
             guard profile.isEmpty == false else { return }
             self.filepath = self.configpath! + macserialnumber! + "/" + profile + "/"
-            self.filename = docuDir + self.configpath! + macserialnumber! + "/" + profile + self.name!
+            self.filename = docuDir + self.configpath! + macserialnumber! + "/" + profile + self.plistname!
         } else {
             // no profile
-            self.filename = docuDir + self.configpath! + macserialnumber! + self.name!
+            self.filename = docuDir + self.configpath! + macserialnumber! + self.plistname!
             self.filepath = self.configpath! + macserialnumber! + "/"
         }
     }
@@ -87,16 +87,16 @@ class ReadWriteDictionary: Setlog {
         self.task = task
         switch self.task! {
         case .schedule:
-            self.name = "/scheduleRsync.plist"
+            self.plistname = "/scheduleRsync.plist"
             self.key = "Schedule"
         case .configuration:
-            self.name = "/configRsync.plist"
+            self.plistname = "/configRsync.plist"
             self.key = "Catalogs"
         case .userconfig:
-            self.name = "/config.plist"
+            self.plistname = "/config.plist"
             self.key = "config"
         case .none:
-            self.name = nil
+            self.plistname = nil
         }
     }
 
