dbf62607667928d945f6992f8da145ae2dfe8d97
Thomas Evensen <thomeven@gmail.com>
2020-07-19 20:22:34 +0200

    Update CompleteScheduledOperation.swift

--- Model/CompleteOperation/CompleteScheduledOperation.swift
+++ Model/CompleteOperation/CompleteScheduledOperation.swift
@@ -11,9 +11,11 @@ import Foundation
 // Class for completion of Operation objects when Process object termination.
 // The object does also kicks of next scheduled job by setting new waiter time.
 final class CompleteScheduledOperation: ScheduledTaskAnimation, SetConfigurations, SetSchedules, Setlog {
-    private var date: Date?
-    private var hiddenID: Int?
-    private var index: Int?
+    weak var reloaddataDelegate: ReloadData?
+    var date: Date?
+    var hiddenID: Int?
+    var index: Int?
+    var dict: NSDictionary?
 
     // Function for finalizing the Scheduled job
     // The Operation object sets reference to the completeScheduledOperation in self.schedules!.operation
@@ -25,13 +27,23 @@ final class CompleteScheduledOperation: ScheduledTaskAnimation, SetConfiguration
         let message = (datestring ?? "") + " " + numberstring
         self.logDelegate?.addlog(logrecord: NSLocalizedString("Adding result to log:", comment: "Logg")
             + " " + numberstring)
-        self.configurations?.setCurrentDateonConfiguration(index: self.index!, outputprocess: outputprocess)
-        _ = Notifications().showNotification(message: message)
+        self.reloaddataDelegate = ViewControllerReference.shared.viewControllermain as? ViewControllerMain
+        if let profilename = self.dict?.value(forKey: "profilename") as? String {
+            if profilename.isEmpty == true {
+                self.reloaddataDelegate?.reloaddata(profilename: nil)
+            } else {
+                self.reloaddataDelegate?.reloaddata(profilename: profilename)
+            }
+        }
+        if let index = self.configurations?.getIndex(hiddenID ?? -1) {
+            self.configurations?.setCurrentDateonConfiguration(index: index, outputprocess: outputprocess)
+            _ = Notifications().showNotification(message: message)
+        }
     }
 
     init(dict: NSDictionary) {
+        self.dict = dict
         self.date = dict.value(forKey: "start") as? Date
-        self.hiddenID = dict.value(forKey: "hiddenID") as? Int ?? -1
-        self.index = self.configurations!.getIndex(hiddenID!)
+        self.hiddenID = dict.value(forKey: "hiddenID") as? Int
     }
 }
