3b965162ab2466e8b13dc54674c4d1949f9896f1
Thomas Evensen <thomeven@gmail.com>
2020-08-28 13:32:58 +0200

    Update NamesandPaths.swift

--- Model/Fileoperations/NamesandPaths.swift
+++ Model/Fileoperations/NamesandPaths.swift
@@ -5,7 +5,7 @@
 //  Created by Thomas Evensen on 28/07/2020.
 //  Copyright © 2020 Thomas Evensen. All rights reserved.
 //
-// swiftlint: disable line_length
+// swiftlint:disable line_length
 
 import Foundation
 
