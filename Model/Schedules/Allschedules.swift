f768866a1675582bc5781241a6c62caa89b1a080
Thomas Evensen <thomeven@gmail.com>
2020-11-04 19:00:40 +0100

    update

--- Model/Schedules/Allschedules.swift
+++ Model/Schedules/Allschedules.swift
@@ -32,6 +32,7 @@ extension GetAllSchedules {
 class Allschedules {
     // Configurations object
     var allschedules: [ConfigurationSchedule]?
+    var schedulesNSDictionary: [NSMutableDictionary]?
     var allprofiles: [String]?
     var alloffsiteservers: [String]?
 
@@ -62,9 +63,31 @@ class Allschedules {
         }
     }
 
+    // Function is reading Schedule plans and transform plans to
+    // array of NSDictionary.
+    // - returns : none
+    private func setallscheduledtasksNSDictionary() {
+        var data = [NSMutableDictionary]()
+        let scheduletypes: Set<String> = [Scheduletype.daily.rawValue, Scheduletype.weekly.rawValue, Scheduletype.once.rawValue]
+        for i in 0 ..< (self.allschedules?.count ?? 0) where
+            self.allschedules?[i].dateStop != nil && scheduletypes.contains(self.allschedules?[i].schedule ?? "")
+        {
+            let dict: NSMutableDictionary = [
+                "dateStart": self.allschedules?[i].dateStart ?? "",
+                "dateStop": self.allschedules?[i].dateStop ?? "",
+                "hiddenID": self.allschedules?[i].hiddenID ?? -1,
+                "schedule": self.allschedules?[i].schedule ?? "",
+                "profilename": self.allschedules?[i].profilename ?? NSLocalizedString("Default profile", comment: "default profile"),
+            ]
+            data.append(dict as NSMutableDictionary)
+        }
+        self.schedulesNSDictionary = data
+    }
+
     init() {
         self.getprofilenames()
         self.alloffsiteservers = [String]()
         self.readallschedules()
+        self.setallscheduledtasksNSDictionary()
     }
 }
