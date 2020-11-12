28e653e5435fd5116db27fbea6e87edf1565ab80
Thomas Evensen <thomeven@gmail.com>
2020-11-12 15:35:22 +0100

    update

--- Model/Schedules/ConfigurationSchedule.swift
+++ Model/Schedules/ConfigurationSchedule.swift
@@ -25,21 +25,53 @@ struct ConfigurationSchedule {
     var profilename: String?
 
     init(dictionary: NSDictionary, log: NSArray?, nolog: Bool) {
-        self.hiddenID = dictionary.object(forKey: "hiddenID") as? Int ?? -1
-        self.dateStart = dictionary.object(forKey: "dateStart") as? String ?? ""
-        self.schedule = dictionary.object(forKey: "schedule") as? String ?? ""
-        self.offsiteserver = dictionary.object(forKey: "offsiteserver") as? String ?? ""
-        if let date = dictionary.object(forKey: "dateStop") as? String { self.dateStop = date }
+        self.hiddenID = dictionary.object(forKey: DictionaryStrings.hiddenID.rawValue) as? Int ?? -1
+        self.dateStart = dictionary.object(forKey: DictionaryStrings.dateStart.rawValue) as? String ?? ""
+        self.schedule = dictionary.object(forKey: DictionaryStrings.schedule.rawValue) as? String ?? ""
+        self.offsiteserver = dictionary.object(forKey: DictionaryStrings.offsiteserver.rawValue) as? String ?? ""
+        if let date = dictionary.object(forKey: DictionaryStrings.dateStop.rawValue) as? String { self.dateStop = date }
         if log != nil, nolog == false {
             for i in 0 ..< (log?.count ?? 0) {
                 if i == 0 { self.logrecords = [Log]() }
                 var logrecord = Log()
                 if let dict = log?[i] as? NSDictionary {
-                    logrecord.dateExecuted = dict.object(forKey: "dateExecuted") as? String
-                    logrecord.resultExecuted = dict.object(forKey: "resultExecuted") as? String
+                    logrecord.dateExecuted = dict.object(forKey: DictionaryStrings.dateExecuted.rawValue) as? String
+                    logrecord.resultExecuted = dict.object(forKey: DictionaryStrings.resultExecuted.rawValue) as? String
                 }
                 self.logrecords?.append(logrecord)
             }
         }
+    }
+}
+
+extension ConfigurationSchedule: Hashable, Equatable {
+    static func == (lhs: ConfigurationSchedule, rhs: ConfigurationSchedule) -> Bool {
+        return lhs.hiddenID == rhs.hiddenID &&
+            lhs.dateStart == rhs.dateStart &&
+            lhs.schedule == rhs.schedule &&
+            lhs.dateStop == rhs.dateStop &&
+            lhs.offsiteserver == rhs.offsiteserver &&
+            lhs.profilename == rhs.profilename
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(String(self.hiddenID))
+        hasher.combine(self.dateStart)
+        hasher.combine(self.schedule)
+        hasher.combine(self.dateStop)
+        hasher.combine(self.offsiteserver)
+        hasher.combine(self.profilename)
+    }
+}
+
+extension Log: Hashable, Equatable {
+    static func == (lhs: Log, rhs: Log) -> Bool {
+        return lhs.dateExecuted == rhs.dateExecuted &&
+            lhs.resultExecuted == rhs.resultExecuted
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.dateExecuted)
+        hasher.combine(self.resultExecuted)
     }
 }
